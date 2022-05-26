9cf42acd4408ad081b811396443ae5880c895b84
RPS98 <rafa.perez.s98@gmail.com>
2022-05-26 11:18:35 +0200

    RCLCPP Info remove

--- lib/speed_controller_plugin.cpp
+++ lib/speed_controller_plugin.cpp
@@ -108,13 +108,7 @@ namespace controller_plugin_speed_controller
       double roll, pitch, yaw;
       m.getRPY(roll, pitch, yaw);
 
-      // Eigen::Quaterniond q(pose_msg.pose.orientation.w, pose_msg.pose.orientation.x,
-      //                      pose_msg.pose.orientation.y, pose_msg.pose.orientation.z);
-
-      // control_ref_.yaw[0] = q.toRotationMatrix().eulerAngles(2, 0, 1)[0];
       control_ref_.yaw[0] = yaw;
-
-      RCLCPP_INFO(node_ptr_->get_logger(), "Yaw reference: %f", yaw);
     }
 
     return;
